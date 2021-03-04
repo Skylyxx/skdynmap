8b3e79b5c541a674f897614b0dfc7e39d7a022a5
Skylyxx <skylyxxfr@gmail.com>
2021-03-04 12:02:56 +0100

    ✨ Introducing chunks support (Thanks to @BakaAless)

---
+++ src/main/java/fr/skylyxx/skdynmap/skript/expressions/area/ExprChunksToLocations.java
@@ -0,0 +1,55 @@
+package fr.skylyxx.skdynmap.skript.expressions.area;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import fr.skylyxx.skdynmap.utils.Util;
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.event.Event;
+
+import javax.annotation.Nullable;
+import java.util.Arrays;
+import java.util.List;
+
+public class ExprChunksToLocations extends SimpleExpression<Location> {
+
+    static {
+        Skript.registerExpression(ExprChunksToLocations.class, Location.class, ExpressionType.SIMPLE,
+                "corners of %chunks%"
+        );
+    }
+
+    private Expression<Chunk> chunkExpr;
+
+    @Override
+    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+        chunkExpr = (Expression<Chunk>) exprs[0];
+        return true;
+    }
+
+    @Override
+    public Class<? extends Location> getReturnType() {
+        return Location.class;
+    }
+
+    @Override
+    public boolean isSingle() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    protected Location[] get(Event e) {
+        List<Location> locations = Util.getCorners(Arrays.asList(chunkExpr.getArray(e).clone()));
+        return locations.toArray(new Location[0]);
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean debug) {
+        return "corners of " + chunkExpr.toString(e, debug);
+    }
+}
